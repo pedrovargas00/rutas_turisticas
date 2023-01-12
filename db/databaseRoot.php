a8f515bd8a8e04d02264bf0e1f76ae58dbc7048d
pedrovargas00 <01vargaspedro@gmail.com>
2023-01-11 20:20:49 -0600

    Terminado

--- db/databaseRoot.php
+++ db/databaseRoot.php
@@ -4,7 +4,7 @@
    $dotenv = Dotenv\Dotenv::createImmutable('./');
    $dotenv->load();
 
-   $conn = mysqli_connect($_ENV['DB_NAME'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_TABLE'])
+   $conn = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_TABLE'])
       or die (mysqli_error($conn));
    mysqli_set_charset($conn, "UTF8");
 ?>
